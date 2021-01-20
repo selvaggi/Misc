c60cf24c4f355813093d7a85f08f1e30c7ce28c1
michele <michele.selvaggi@cern.ch>
2021-01-20 17:28:55 +0100

    added latest version

--- stock/boll.py
+++ stock/boll.py
@@ -78,101 +78,141 @@ def compute_risk(data, ndays):
 
 #-------------------------------------------------------------------------------
 
-ema_period1 = 20             # change it to ema_period = 30 for your case
-ema_period2 = 350             # change it to ema_period = 30 for your case
-tick='BTC-USD'
+def plot_security(ticker, ema1, ema2):
 
-data = yf.download(tickers=tick, period='max', interval='1d')
+    ema_period1 = ema1          # change it to ema_period = 30 for your case
+    ema_period2 = ema2            # change it to ema_period = 30 for your case
+    tick=ticker
 
-
-#Draw the middle band, higher band, lowest band
-#data['Middle Band'] = data['Close'].rolling(window=21).mean()
-#data['Upper Band'] = data['Middle Band'] + 1.96*data['Close'].rolling(window=21).std()
-#data['Lower Band'] = data['Middle Band'] - 1.96*data['Close'].rolling(window=21).std()
-
-#data['50dayMA'] = data['Middle Band'] - 1.96*data['Close'].rolling(window=21).std()
-
-print(data.head(40))
-
-df = data
-
-myalpha1 = 2/(ema_period1+1)
-myalpha2 = 2/(ema_period2+1)
-
-# concise form : df.expanding(min_periods=12).mean()
-df['Expand_Mean1'] = data['Close'].rolling(window=len(df), min_periods=ema_period1).mean()
-# obtain the very first index after nulls
-idx = df['Expand_Mean1'].first_valid_index()
-# Make all the subsequent values after this index equal to NaN
-df.loc[idx:, 'Expand_Mean1'].iloc[1:] = np.NaN
-# Let these rows now take the corresponding values in the Close column
-df.loc[idx:, 'Expand_Mean1'] = df['Expand_Mean1'].combine_first(df['Close'])
-# Perform EMA by turning off adjustment
-df['EMA1'] = df['Expand_Mean1'].ewm(alpha=myalpha1, adjust=False).mean()
-
-# concise form : df.expanding(min_periods=12).mean()
-df['Expand_Mean2'] = data['Close'].rolling(window=len(df), min_periods=ema_period2).mean()
-# obtain the very first index after nulls
-idx = df['Expand_Mean2'].first_valid_index()
-# Make all the subsequent values after this index equal to NaN
-df.loc[idx:, 'Expand_Mean2'].iloc[1:] = np.NaN
-# Let these rows now take the corresponding values in the Close column
-df.loc[idx:, 'Expand_Mean2'] = df['Expand_Mean2'].combine_first(df['Close'])
-# Perform EMA by turning off adjustment
-df['EMA2'] = df['Expand_Mean2'].ewm(alpha=myalpha2, adjust=False).mean()
+    data = yf.download(tickers=tick, period='max', interval='1d')
 
 
-##
+    #Draw the middle band, higher band, lowest band
+    #data['Middle Band'] = data['Close'].rolling(window=21).mean()
+    #data['Upper Band'] = data['Middle Band'] + 1.96*data['Close'].rolling(window=21).std()
+    #data['Lower Band'] = data['Middle Band'] - 1.96*data['Close'].rolling(window=21).std()
+
+    #data['50dayMA'] = data['Middle Band'] - 1.96*data['Close'].rolling(window=21).std()
 
 
-df['risk'] = df['EMA1'].div(df['EMA2'])
-df['norm. risk'] = df['risk'].div(df['risk'].max())
+    df = data
+
+    myalpha1 = 2/(ema_period1+1)
+    myalpha2 = 2/(ema_period2+1)
+
+    # concise form : df.expanding(min_periods=12).mean()
+    df['Expand_Mean1'] = data['Close'].rolling(window=len(df), min_periods=ema_period1).mean()
+    # obtain the very first index after nulls
+    idx = df['Expand_Mean1'].first_valid_index()
+    # Make all the subsequent values after this index equal to NaN
+    df.loc[idx:, 'Expand_Mean1'].iloc[1:] = np.NaN
+    # Let these rows now take the corresponding values in the Close column
+    df.loc[idx:, 'Expand_Mean1'] = df['Expand_Mean1'].combine_first(df['Close'])
+    # Perform EMA by turning off adjustment
+    df['EMA1'] = df['Expand_Mean1'].ewm(alpha=myalpha1, adjust=False).mean()
+
+    # concise form : df.expanding(min_periods=12).mean()
+    df['Expand_Mean2'] = data['Close'].rolling(window=len(df), min_periods=ema_period2).mean()
+    # obtain the very first index after nulls
+    idx = df['Expand_Mean2'].first_valid_index()
+    # Make all the subsequent values after this index equal to NaN
+    df.loc[idx:, 'Expand_Mean2'].iloc[1:] = np.NaN
+    # Let these rows now take the corresponding values in the Close column
+    df.loc[idx:, 'Expand_Mean2'] = df['Expand_Mean2'].combine_first(df['Close'])
+    # Perform EMA by turning off adjustment
+    df['EMA2'] = df['Expand_Mean2'].ewm(alpha=myalpha2, adjust=False).mean()
 
 
-data=df
-
-#print(sma(mylist, N))
-#compute_sma(data, long_period)
-#compute_risk(data, long_period)
-
-print(data.head(40))
-
-#print(data['Middle Band'])
+    ##
 
 
-# Create figure with secondary y-axis
-fig = make_subplots(specs=[[{"secondary_y": True}]])
-#declare figure
-
-#fig.add_trace(go.Scatter(x=data.index, y= data['Middle Band'],line=dict(color='blue', width=.7), name = 'Middle Band'))
-#fig.add_trace(go.Scatter(x=data.index, y= data['Upper Band'],line=dict(color='red', width=1.5), name = 'Upper Band (Sell)'))
-#fig.add_trace(go.Scatter(x=data.index, y= data['Lower Band'],line=dict(color='green', width=1.5), name = 'Lower Band (Buy)'))
-#fig.add_trace(go.Scatter(x=data.index, y= data['SMA_50'],line=dict(color='dark blue', width=1.5), name = 'SMA50'))
-#fig.add_trace(go.Scatter(x=data.index, y= data['SMA_350'],line=dict(color='blue', width=1.5), name = 'SMA350'))
-
-#Candlestick
-fig.add_trace(go.Candlestick(x=data.index,
-                open=data['Open'],
-                high=data['High'],
-                low=data['Low'],
-                close=data['Close'], name = 'market data'),
-                secondary_y=False,
-)
-
-fig.add_trace(go.Scatter(x=data.index, y= data['norm. risk'],line=dict(color='blue', width=1.5), name = 'risk'), secondary_y=True)
+    df['risk'] = df['EMA1'].div(df['EMA2'])
+    df['norm. risk'] = df['risk'].div(df['risk'].max())
 
 
-# Add titles
-fig.update_layout(
-    title='SPY live share price evolution',
-    yaxis_title='Stock Price (USD per Shares)')
+    data=df
+
+    #print(sma(mylist, N))
+    #compute_sma(data, long_period)
+    #compute_risk(data, long_period)
+
+    #print(data.head(40))
+
+    #print(data['Middle Band'])
+
+
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    #declare figure
+
+    #fig.add_trace(go.Scatter(x=data.index, y= data['Middle Band'],line=dict(color='blue', width=.7), name = 'Middle Band'))
+    #fig.add_trace(go.Scatter(x=data.index, y= data['Upper Band'],line=dict(color='red', width=1.5), name = 'Upper Band (Sell)'))
+    #fig.add_trace(go.Scatter(x=data.index, y= data['Lower Band'],line=dict(color='green', width=1.5), name = 'Lower Band (Buy)'))
+    #fig.add_trace(go.Scatter(x=data.index, y= data['SMA_50'],line=dict(color='dark blue', width=1.5), name = 'SMA50'))
+    #fig.add_trace(go.Scatter(x=data.index, y= data['SMA_350'],line=dict(color='blue', width=1.5), name = 'SMA350'))
+
+    #Candlestick
+    fig.add_trace(go.Candlestick(x=data.index,
+                    open=data['Open'],
+                    high=data['High'],
+                    low=data['Low'],
+                    close=data['Close'], name = 'market data'),
+                    secondary_y=False,
+    )
+
+    fig.add_trace(go.Scatter(x=data.index, y= data['norm. risk'],line=dict(color='blue', width=1.5), name = 'risk'), secondary_y=True)
+
+
+    # Add titles
+    fig.update_layout(
+        title='{} live share price evolution'.format(tick),
+        yaxis_title='Stock Price (USD per Shares)')
 
 
 
-# Set y-axes titles
-fig.update_yaxes(title_text="price", secondary_y=False, type="log")
-fig.update_yaxes(title_text="risk", secondary_y=True)
+    # Set y-axes titles
+    fig.update_yaxes(title_text="price", secondary_y=False, type="log")
+    #fig.update_yaxes(title_text="price", secondary_y=False)
+    fig.update_yaxes(title_text="risk", secondary_y=True)
 
 
-#Show
-fig.show()
+    print('--------------- ',tick,' ---------------')
+    print('  ')
+    print(data.tail(10))
+
+
+    #Show
+    fig.show()
+
+coins0 = [
+    'BTC-USD',
+]
+
+coins1 = [
+    'BTC-USD',
+    'ETH-USD',
+    'LINK-USD',
+    'ADA-USD',
+    'DOT-USD',
+]
+
+coins2 = [
+    'BTC-USD',
+    'ETH-USD',
+    'LINK-USD',
+    'ADA-USD',
+    'XMR-USD',
+    'LTC-USD',
+    'XRP-USD',
+    'BCH-USD',
+    'EOS-USD',
+    'ZEC-USD'
+]
+
+stocks = [
+    'TSLA',
+]
+
+
+for coin in coins1:
+    plot_security(coin, 20, 350)
